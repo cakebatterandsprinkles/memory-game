5f05b030f74f0388f2787df1666a393625938f30
yagmurcetintas <yagmurcetin@gmail.com>
2019-07-30 10:01:31 -0400

    added sticky navbar

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,12 +3,12 @@ import "./Title.css"
 
 function Title(props) {
     return <nav
-        class="navbar navbar-expand-lg navbar-light bg-info justify-content-between">
+        className="navbar nav fixed-top navbar-light bg-info justify-content-between sticky">
         <div className="container">
-            <span class="text-light">{props.children}</span>
-            <span class="text-light">{props.message}</span>
-            <span class="text-light">Top Score: {props.topScore}</span>
-            <span class="text-light">Score: {props.score}</span>
+            <span className="text-light">{props.children}</span>
+            <span className="text-light">{props.message}</span>
+            <span className="text-light">Top Score: {props.topScore}</span>
+            <span className="text-light">Score: {props.score}</span>
         </div>
     </nav>;
 }
